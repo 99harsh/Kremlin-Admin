d0e28ad071358c84a6de26b1d8e380c0734e4a46
Harsh Agrawal <harshagrawal@Harshs-MacBook-Air.local>
2023-04-18 22:58:54 +0530

    Git Updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false,
-    url:"http://93.188.161.127:3000/"
+    production: true,
+    url:"http://93.188.161.127:3030/"
 };
 
 /*
